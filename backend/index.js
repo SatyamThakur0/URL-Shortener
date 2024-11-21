6f59a54c8faa7b2bd5d4764ac7b76a2aeea73424
Satyam Thakur <sk165344@gmail.com>
2024-11-21 19:00:57 +0530

    preflight

--- backend/index.js
+++ backend/index.js
@@ -9,7 +9,7 @@ dotenv.config({});
 
 // Middleware
 app.use(express.json());
-app.use(cors());
+// app.use(cors());
 
 app.use(
     cors({
@@ -18,6 +18,7 @@ app.use(
         credentials: true,
     })
 );
+app.options("*", cors());
 
 // Connect to MongoDB
 mongoose
